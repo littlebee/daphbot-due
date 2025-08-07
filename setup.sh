a744ffa2fc277bd38404f785c44bfb835e401fcd
Bee <bee.wilkerson@ymail.com>
2025-08-07 07:41:37 -0700

    Add setup for auto starting daphbbot services from systemd

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# to automatically run our start script on desktop login
+cp setup_files/home/.config/systemd/user/daphbot.service ~/.config/systemd/user/
+systemctl --user enable daphbot.service
--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+source ~/bb_env/bin/activate
+export BB_ENV=production
+
 bb_start $@
