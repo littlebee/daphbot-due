7ac69658e9fa34d20fdaa925b7a13960555bebbe
Bee <bee.wilkerson@ymail.com>
2025-09-21 08:42:37 -0700

    linting polish
    I didn't notice these before merging the lint PR.  the call to mypy in lint.sh can be simplified:
    1. don't need PYTHONPATH=src for mypy.  that was something Claude hallucinated
    2. command line switch added should be in mypy.ini

--- lint.sh
+++ lint.sh
@@ -7,5 +7,5 @@ echo "Linting with flake8..."
 python -m flake8 src
 
 echo "Running mypy (typechecker): $(python -m mypy --version)"
-PYTHONPATH=src python -m mypy --explicit-package-bases src
+python -m mypy src
 
